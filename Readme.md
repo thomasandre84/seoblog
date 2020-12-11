9a8379012a9cb9d2d7bd787b45423afe129ade39
Thomas Andre <thomasandre84@gmail.com>
2020-12-11 18:37:19 +0100

    db setup and controller work

--- Readme.md
+++ Readme.md
@@ -1,3 +1,21 @@
 # Videos Packtpub
 
 - https://subscription.packtpub.com/video/web_development/9781800206274/p2/video2_4/-database-setup-locally
+
+## Setup mongo
+Within the Mongo-Shell as Admin
+````
+> use SEOBlog
+switched to db SEOBlog
+> use admin
+switched to db admin
+> db.createUser(
+...   {
+...     user: "seoblog",
+...     pwd: "seoblog",
+...     roles: [
+...        { role: "readWrite", db: "seoblog" }
+...      ]
+...   }
+... )
+````
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,18 @@ const morgan = require('morgan')
 const bodyparser = require('body-parser')
 const cookieparser= require('cookie-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
+// bring routes
+const blogRoutes = require('./routes/blog');
 
 // app
 const app = express()
+
+// db
+mongoose
+.connect(process.env.DATABASE, {useNewUrlParser: true, useCreateIndex: true ,useFindAndModify: false, useUnifiedTopology: true })
+.then(() => console.log('Database Connected'));
 
 // middleware
 app.use(morgan('dev'))
@@ -17,10 +25,9 @@ if(process.env.NODE_ENV == 'development') {
     app.use(cors({origin: `${process.env.CLIENT_URL}`}));
 }
 
-// routes
-app.get('/api', (req, res) => {
-    res.json({time: Date().toString()});
-});
+// routes middleware
+app.use('/api', blogRoutes);
+
 
 // port
 const port = process.env.PORT || 8000
